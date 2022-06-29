2ed53bf21419dc2051b5314a7cb56f40e7cf7541
XenoWars <43147270+XenoWars@users.noreply.github.com>
2022-06-29 18:13:34 -0400

    Initial setup

---
+++ scripts/init.lua
@@ -0,0 +1,6 @@
+Tracker:AddItems("items/wheel.json")
+Tracker:AddItems("items/equipment.json")
+Tracker:AddItems("items/misc.json")
+
+Tracker:AddLayouts("tracker.json")
+Tracker:AddLayouts("broadcast.json")
