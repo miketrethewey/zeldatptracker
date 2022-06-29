26c1f3f8e93d17d1c545503c649367abe4bd1c0c
Mike A. Trethewey <miketrethewey@gmail.com>
2022-06-29 16:13:22 -0700

    Satisfy Items-Only
    Fix key in Broadcast Layout
    Fix Hidden Skills
    Make Progressives rollover
    Dummy Locations file
    World-level Locations file
    Dummy Maps file
    Smarter Init file
    Preserve settings from initial Layout file
    Fix key in Tracker Layout
    Delete redundant files from Map Variant

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,6 +1,40 @@
+local variant = Tracker.ActiveVariantUID
+if variant == "" then
+  variant = "items_only"
+end
+
+-- Items
+print("Loading Items")
 Tracker:AddItems("items/wheel.json")
 Tracker:AddItems("items/equipment.json")
 Tracker:AddItems("items/misc.json")
+print("")
 
+-- Other Layouts
+print("Loading Layouts")
 Tracker:AddLayouts("tracker.json")
 Tracker:AddLayouts("broadcast.json")
+print("")
+
+if string.find(variant, "map") then
+  print("Map Variant; load map stuff")
+  print("")
+
+  -- Options Layout
+  print("Loading Options")
+  Tracker:AddLayouts("options.json")
+  print("")
+
+  print("Loading Maps")
+  -- Overworld Map
+  Tracker:AddMaps("maps/maps.json")
+  print("")
+else
+  print("Not a Map Variant; load default stuff")
+end
+
+-- Legacy
+print("Satisfy Legacy Loads")
+Tracker:AddMaps("maps/maps.json")
+Tracker:AddLocations("locations/world.json")
+print("")
